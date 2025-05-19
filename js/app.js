50cc51a4dbebf9a33da84a646ff452e3a0e308e8
ViniciusFreiry <viniciusfreiry@gmail.com>
2025-05-19 15:31:18 -0300

    Scaling Adjustments

--- js/app.js
+++ js/app.js
@@ -1,27 +1,7 @@
-const game1 = document.getElementById("game-1");
-const game2 = document.getElementById("game-2");
-const game3 = document.getElementById("game-3");
-
-function alterarStatus(game) {
-    let container;
-    let button;
-
-    switch(game) {
-        case 1:
-            container = game1.querySelector("div");
-            button = game1.querySelector("a");
-        break;
-
-        case 2: 
-            container = game2.querySelector("div");
-            button = game2.querySelector("a");
-        break;
-
-        case 3:
-            container = game3.querySelector("div");
-            button = game3.querySelector("a");
-        break;
-    }
+function alterarStatus(id) {
+    const game = document.getElementById(`game-${id}`);
+    const container = game.querySelector("div");
+    const button = game.querySelector("a");
 
     if (container.classList.contains("dashboard__item__img--rented")) {
         container.classList.remove("dashboard__item__img--rented");
